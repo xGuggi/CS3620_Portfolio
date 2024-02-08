36e054984450e4fb0ac7edb4605065399cfab6ca
Spencer Guggisberg <spencerguggi@gmail.com>
2024-02-08 15:57:36 -0700

    Third commit

--- Portfolio/PortfolioDatabase/urls.py
+++ Portfolio/PortfolioDatabase/urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home/', views.Home, name='home'),
-    path('hobbies/', views.Hobbies, name='hobbies'),
-    path('portfolio/', views.Portfolio, name='portfolio'),
-    path('contact/', views.Contact, name='contact')
+    path('home/', views.home, name='home'),
+    path('hobbies/', views.hobbies, name='hobbies'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('contact/', views.contact, name='contact')
 ]
--- Portfolio/PortfolioDatabase/views.py
+++ Portfolio/PortfolioDatabase/views.py
@@ -5,21 +5,21 @@ from .models import Portfolio
 
 
 # Create your views here.
-def Home(request):
+def home(request):
     return HttpResponse("Welcome to my Portfolio! In the dynamic landscape of technology, the pursuit of knowledge in "
                         "computer science has become an exhilarating journey. As a senior at Weber State University, "
                         "I have had the privilege of delving into the intricacies of this ever-evolving field.  ")
 
 
-def Hobbies(request):
+def hobbies(request):
     hobbies_list = Hobbies.objects.all()
     return HttpResponse(hobbies_list)
 
 
-def Portfolio(request):
+def portfolio(request):
     portfolio_list = Portfolio.objects.all()
     return HttpResponse(portfolio_list)
 
 
-def Contact(request):
+def contact(request):
     return HttpResponse("Student Email: spencerguggisberg@mail.weber.edu")
