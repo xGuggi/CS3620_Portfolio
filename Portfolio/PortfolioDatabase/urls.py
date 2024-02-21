fde9374bd54a21e525b78a29f24c86101d5e2a61
Spencer Guggisberg <spencerguggi@gmail.com>
2024-02-20 19:21:23 -0700

    Final Submit

--- Portfolio/PortfolioDatabase/urls.py
+++ Portfolio/PortfolioDatabase/urls.py
@@ -1,10 +1,11 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<int:hobbies_id>', views.detail_hobbies, name='detail_hobbies'),
-    path('<int:portfolio_id>', views.detail_portfolio, name='detail_portfolio'),
+    path('hobbies/<int:hobbies_id>/', views.detail_hobbies, name='detail_hobbies'),
+    path('portfolio/<int:portfolio_id>/', views.detail_portfolio, name='detail_portfolio'),
     path('hobbies/', views.hobbies, name='hobbies'),
     path('portfolio/', views.portfolio, name='portfolio'),
     path('contact/', views.contact, name='contact'),
